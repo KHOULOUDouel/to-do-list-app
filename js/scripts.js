6f8c6b5ef76d9598eac629b9a12c11101a769c4e
KHOULOUDouel <160501993+KHOULOUDouel@users.noreply.github.com>
2024-03-31 03:43:59 +0200

    Using jQuery selectors and event listeners

---
+++ js/scripts.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+    // Event listener for adding a new item to the list
+    $('#button').on('click', function() {
+        var deleteButton = $('<button></button>').addClass('delete').text('X');
+        var newItemValue = $('input[name="ListItem"]').val();
+        if (newItemValue !== '') {
+            var newItem = $('<li></li>').text(newItemValue).append(deleteButton);
+            $('#list').append(newItem);
+            $('input[name="ListItem"]').val('');
+        }
+    });
+
+    // Event listener for marking an item off the list
+    $('#list').on('dblclick', 'li', function() {
+        $(this).toggleClass('strike');
+    });
+
+    // Event listener for deleting an item from the list
+    $('#list').on('click', '.delete', function(event) {
+        $(this).parent().remove();
+        event.stopPropagation();
+    });
+
+    // Make list items sortable
+    $('#list').sortable();
+
+    // Example of creating and appending a new <li> element to a list with the class "selectedElement"
+    $('.selectedElement').append('<li>some item</li>');
+});
