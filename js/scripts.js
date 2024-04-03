ce6e8e395f428b26216ac21cc631d4fd16c29fef
KHOULOUDouel <160501993+KHOULOUDouel@users.noreply.github.com>
2024-04-03 22:05:18 +0200

    Ensure that all the code is contained within the function

--- js/scripts.js
+++ js/scripts.js
@@ -1,37 +1,33 @@
-$(document).ready(function() {
-    // Event listener for adding a new item to the list
-    $('#button').on('click', function() {
-        var deleteButton = $('<button></button>').addClass('delete').text('X');
-        var newItemValue = $('input[name="ListItem"]').val();
-        if (newItemValue !== '') {
-            var newItem = $('<li></li>').text(newItemValue).append(deleteButton);
-            $('#list').append(newItem);
-            $('input[name="ListItem"]').val('');
-        }
-    });
-
-    // Event listener for marking an item off the list
-    $('#list').on('dblclick', 'li', function() {
-        $(this).toggleClass('strike');
-    });
-
-    // Event listener for deleting an item from the list
-    $('#list').on('click', '.delete', function(event) {
-        $(this).parent().remove();
-        event.stopPropagation();
-    });
-
-    // Make list items sortable
-    $('#list').sortable();
-});
-
 // Define the newItem function
 function newItem() {
-    var deleteButton = $('<button></button>').addClass('delete').text('X');
-    var newItemValue = $('input[name="ListItem"]').val();
-    if (newItemValue !== '') {
-        var newItem = $('<li></li>').text(newItemValue).append(deleteButton);
-        $('#list').append(newItem);
-        $('input[name="ListItem"]').val('');
-    }
+  let listItem = $("<li></li>");
+  let listItemValue = $("#input").val();
+  listItem.text(listItemValue);
+
+  if (listItemValue === "") {
+    alert("You must write something!");
+  } else {
+    $("#list").append(listItem);
+  }
+
+  function crossOut() {
+    listItem.toggleClass("strike");
+  }
+
+  // Event listener for marking an item off the list
+  listItem.on("dblclick", crossOut);
+
+  let crossOutButton = $("<crossOutButton></crossOutButton>");
+  crossOutButton.text("X");
+  listItem.append(crossOutButton);
+
+  function deleteListItem() {
+    listItem.addClass("delete");
+  }
+
+  // Event listener for deleting an item from the list
+  crossOutButton.on("click", deleteListItem);
+
+  // Make list items sortable
+  $("#list").sortable();
 }
